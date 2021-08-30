df6b636b4e6323cd0c5066d50bbac79b1778ed68
root <root@Orange.localdomain>
2021-08-30 13:18:42 +0200

    works well before adding 2D array to objects.skake

--- logic/main.js
+++ logic/main.js
@@ -4,20 +4,40 @@ const popUpStart_div = document.getElementById('popUpStart');
 const popUpEnd_div = document.getElementById('popUpEnd');
 
 
-
+// Listen for space bar and start game
 document.addEventListener('keyup', event => {
     if (event.code === 'Space') {
-        popUp(popUpStart_div, 'hide');
-        gameStarted = true;
-        console.log('game started!!');
+        if(gameIsOver === false) {
+            popUp(popUpStart_div, 'hide');
+            gameOnOff('on');
+        } else if(gameIsOver === true ) {
+            popUp(popUpEnd_div, 'hide');
+        }
+
+            
+            
+            
+
+            
+
+          
+            
+            
+    
+        
     }
   });
-//hide pop ups
+//hide pop-ups
 function popUp(element, action) {
+    console.log('popup' + action);
     if(action === 'show') {
-        element.style.display = 'flex';
+        console.log(element + action + 'show');
+        element.style.visibility = "visible";
+        console.log(element.style.visibility);
     } else if( action === 'hide') {
-        element.style.display = 'none';
+        console.log(element + action + 'hide');
+        element.style.visibility = "hidden";
+        console.log(element.style.visibility);
     }
     
 }
@@ -26,8 +46,10 @@ function popUp(element, action) {
 let array484 = [];
 let boardBluePrint = [];
 let actionBluePrint = [];
-let movement = 1;
+let movement = 1;       // up = 1, right = 2, down = 3, left = 4
 let gameStarted = false;
+let gameIsOver = false;
+let eatFood = true;
 
 // gallery of objects
 let foodLocation;
@@ -49,11 +71,11 @@ const createArray484 = () => {
 };
 
 
-//create boardBluePrint
+//create boardBluePrint. return an array with infos for walls, empty, snake and food
 let createBoardBluePrint = () => {
     let arr = [];
     for(let i = 0; i < 484; i++ ){
-      if(array484[i] < 22 || array484[i] > 461 || array484[i] % 22 === 0 || array484[i] % 22 === 21 ){
+      if(checkIfWall(array484[i])) {
         arr.push(wall);
       } else{
         arr.push(empty);
@@ -63,29 +85,52 @@ let createBoardBluePrint = () => {
   }
 
 //render objects with an object
+//snake has 2 elements: position and movement
 let objects = {
     snake: [230, 252, 274],
     food: undefined,
+}
+//verify if tile is wall. Return true if index is a wall, false if it's not
+function checkIfWall(index) {
+    if(index < 22 || index > 460 || index % 22 === 0 || index % 22 === 21) {
+        return true
+    } else {
+        return false;
+    }
+}
+// check if index is on snake
+function checkIfSnake(index) {
+    for(let i = 0; i < objects.snake.length; i++) {
+        if(index == objects.snake[i]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 //place food and bombs at start
 function placeFood() {
     do{
         objects.food = Math.floor(Math.random() * (460 - 23)) + 23;
-      } while(objects.food % 22 === 0 || objects.food % 22 === 21)
-    let conflict = false;
-    for(let i = 0; i < objects.snake.length; i++){
-        if(objects.food == objects.snake[i]){
-            placeFood();
-        }
+        console.log('food = ' + objects.food);
+      } while(checkIfWall(objects.food));
+    if (checkIfSnake(objects.food)) {
+        placeFood();
+    } else {
+        return
     }
 }
 
 
 //function gameRenderingBluePrint
-function createActionBluePrint(boardBluePrint) {
-    placeFood();
-    let arr = boardBluePrint;
+function createActionBluePrint() {
+    
+    let arr = createBoardBluePrint();
+    if(eatFood === true){
+        placeFood();
+        eatFood = false;
+    }
     arr[objects.food] = food;
     //render snake
     for(let i = 0; i < objects.snake.length; i++){
@@ -100,11 +145,11 @@ function createActionBluePrint(boardBluePrint) {
     return arr;
 }
 
-//export action to the DOM
+//export action to the DOM.
 
 let exportToDom = (actionBluePrint) => {
     for(let i = 0; i < 484; i++) {
-        if(i < 22 || i > 460 || i % 22 == 0 || i % 22 == 21){
+        if(checkIfWall(i)){
             let newDiv = document.createElement('div');
             newDiv.classList.add('wall', 'tile');
             gameBoard_div.appendChild(newDiv);
@@ -131,75 +176,100 @@ let exportToDom = (actionBluePrint) => {
         }
     }
 }
-// game over, pop up, serpent disparait
 
+
+// game over, pop up, serpent disparait
 function gameOver() {
     popUp(popUpEnd_div, 'show');
+    gameIsOver = true;
 }
 
+
+
 // contrôle du serpent avec les flèches du clavier
-    //left = 37
-    //up = 38
-    //right = 39
-    //down = 40
-
-
+    //left = event.key === "ArrowUp"
+    //up = event.key === "ArrowLeft"
+    //right = event.key === "ArrowRight"
+    //down = event.key === "ArrowDown"
 document.addEventListener('keydown', event => {
-    if(gameStarted === true) {
+    if(gameIsOver === false) {
         if (event.key === "ArrowUp") {
-            console.log('up');
             movement = 1;
-            snakeMoving(objects.snake, 1);
         } else if (event.key === "ArrowDown") {
-            console.log('Down');
             movement = 3;
-            snakeMoving(objects.snake);
         } else if (event.key === "ArrowRight") {
-            console.log('Right');
             movement = 2;
-            snakeMoving(objects.snake);
         } else if (event.key === "ArrowLeft") {
-            console.log('Left');
             movement = 4;
-            snakeMoving(objects.snake);
         }
-
     }
-    
-    
-  });
+
+});
+
+//check if game over
+function checkIfGameOver(newTile) {
+    if(newTile < 22 || newTile > 460 || newTile % 22 === 0 || newTile % 22 === 21){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
-function snakeMoving(snake, move) {
-    
+  //update the snake array: activated by user or by setInterval(). 
+  //move receives arrow key or movement variable
+function updateSnake() {
+    console.log('start of snake update: ' + objects.snake[0]);
     let newTile;
     //snake going up
-    if(move === 1) {
-        newTile = snake[0] - 22;
+    if(movement === 1) {
+        newTile = objects.snake[0] - 22;
+        console.log('new tile going up ' + newTile);
     }
     //snake going right
-    else if(move === 2) {
-        newTile = snake[0]++;
-    }
-    //snake going left
-    else if(move === 4) {
-        newTile = snake--;
+    else if(movement === 2) {
+        newTile = objects.snake[0] + 1;
+        console.log('new tile going right ' + newTile);
     }
     //snake going down
-    else if(move === 3){
-        newTile = snake + 22;
+    else if(movement === 3){
+        newTile = objects.snake[0] + 22;
+        console.log('new tile going down ' + newTile);
     }
-    if(newTile < 22 || newTile > 460 || newTile % 22 === 0 || newTile % 22 === 21){
-        console.log('game over');
+    //snake going left
+    else if(movement === 4) {
+        newTile = objects.snake[0] - 1;
+        console.log('new tile going left ' + newTile);
+    }
+
+    if(checkIfGameOver(newTile)) {
+        gameOver();
+        gameOnOff('off');
     } else {
-        console.log(snake);
-        snake.unshift(newTile);
-        snake.pop();
-        console.log(snake);
-        createActionBluePrint(boardBluePrint);
-        while(gameBoard_div.lastElementChild) {
-            gameBoard_div.removeChild(gameBoard_div.lastElementChild);
-        }
+
+        objects.snake.unshift(newTile);
+        objects.snake.pop();
+        console.log('end of snake update' + objects.snake[0]);
+    }
+    
+}
+
+// start game, make the snake move
+let newFrameTimer;
+let gameIsOn = false;
+function gameOnOff(e) {
+    
+    if(e === 'on') {
+        renderNewFrame();
+        newFrameTimer = setInterval(renderNewFrame, 100);
+    } else if (e === 'off'){
+        clearInterval(newFrameTimer);
+    }
+    
+    function renderNewFrame() {
+        updateSnake();
+        actionBluePrint = createActionBluePrint();
+        clearBoard();
         exportToDom(actionBluePrint);
     }
 }
@@ -215,16 +285,20 @@ function snakeMoving(snake, move) {
 
 
 
-
-  //initializing game
+  //initializing game on page load. Empty board with pop up message
 function initialiseGame() {
     array484 = createArray484();
-    console.log(array484);
     boardBluePrint = createBoardBluePrint();
-    console.log(boardBluePrint);
-    actionBluePrint = createActionBluePrint(boardBluePrint);
-    exportToDom(actionBluePrint);
-    
+    exportToDom(boardBluePrint);
 }
 
 initialiseGame();
+
+
+// clear board before rendering new frame
+function clearBoard() {
+    while(gameBoard_div.lastElementChild) {
+        gameBoard_div.removeChild(gameBoard_div.lastElementChild);
+    }
+}
+
